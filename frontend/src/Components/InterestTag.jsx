c4e8b6062c93f556c642f3b7b052163008682762
Vishnu Alachi <vishnualachi@gmail.com>
2025-11-14 11:14:46 -0800

    added map component and event component, not done

---
+++ frontend/src/Components/InterestTag.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./InterestTag.css";
+
+
+export default function TagComponent(props) {
+
+    const className = `Tag-Container ${props.Interest.trim().toLowerCase()}`;
+
+  return (
+    <div className={className}>
+      <div className="Tag-Text">{props.Interest}</div>
+    </div>
+  );
+}
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,31 +1,92 @@
 import React, { useState } from "react";
 import "./HomePage.css";
-import SmallMapComponent from '../Components/SmallMapComponent.jsx'
-import RandomComponent from "../Components/RandomComponent.jsx";
+import MainMapComponent from '../Components/MainMapComponent.jsx'
+import RandomComponent from "../Components/EventComponent.jsx";
 
 
 const test_Event_List = [
   {
-    eventName : "EventName",
+    eventName : "basketball game",
+    eventTime: "5pm",
+    eventAddress : "PAC",
     description : "Description",
     attendees : "Attendees",
-    Host : "Host"
-  }
+    Host : "Host",
+    Interest : "sports",
+  },
+  {
+    eventName : "Drawing Club",
+    eventTime: "5pm",
+    eventAddress : "Room 205",
+    description : "Description",
+    attendees : "Attendees",
+    Host : "Host",
+    Interest : "art",
+  },
+  {
+    eventName : "Jazz band",
+    eventTime: "5pm",
+    eventAddress : "PAC",
+    description : "Description",
+    attendees : "Attendees",
+    Host : "Host",
+    Interest : "music",
+  },
+  {
+    eventName : "Foreign Exchange club",
+    eventTime: "5pm",
+    eventAddress : "Dexter Lawn",
+    description : "Description",
+    attendees : "Attendees",
+    Host : "Host",
+    Interest : "travel",
+  },
+  {
+    eventName : "hackathon",
+    eventTime: "5pm",
+    eventAddress : "PAC",
+    description : "Description",
+    attendees : "Attendees",
+    Host : "Host",
+    Interest : "coding",
+  },
+  {
+    eventName : "Soccer Game",
+    eventTime: "5pm",
+    eventAddress : "lower field",
+    description : "Description",
+    attendees : "Attendees",
+    Host : "Host",
+    Interest : "sports",
+  },
+
 ]
 
 export default function HomePage() {
   return (
-    <div>
-      <h1> map Component </h1>
-      <p> this is a small map component to get the locaiton of the user </p>
-      <SmallMapComponent />
-
-      <RandomComponent 
-      eventName = "Event Name"
-      description = "Description"
-      attendees = "Attendees"
-      Host = "Host"
-      />
+    <div className="HomePage">
+      <div className="Event_Container">
+        <div className="Search_Bar">
+          <input placeholder="Search events…" />
+        </div>
+        <div className="Event_List">
+        {test_Event_List.map((event, index) => (
+          <RandomComponent
+            key={index}
+            eventName={event.eventName}
+            eventTime={event.eventTime}
+            eventAddress={event.eventAddress}
+            description={event.description}
+            attendees={event.attendees}
+            host={event.host}
+            interest={event.Interest}
+          />
+        ))}
+        </div>
+      </div>
+      <div className="Map">
+        <MainMapComponent/>
+      </div>
     </div>
-  )
+  );
 }
