5ac5f572a88ddfe1588766f981b41f663ca3af07
Vishnu Alachi <vishnualachi@gmail.com>
2026-01-20 22:20:27 -0800

    changes

--- test/setupTests.cjs
+++
@@ -1,9 +0,0 @@
-require('@testing-library/jest-dom');
-
-const mockGeolocation = {
-  getCurrentPosition: jest.fn(),
-  watchPosition: jest.fn(),
-  clearWatch: jest.fn(),
-};
-
-global.navigator.geolocation = mockGeolocation;
