434b2053d772433d27636cd024f92ba21739186f
Vishnu Alachi <vishnualachi@gmail.com>
2026-01-23 09:37:39 -0800

    added event creation feature to home page

---
+++ backend/User.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    dateOfBirth: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    interests: {
+      type: [{ type: String }],
+      required: false,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    location: {
+      latitude: {
+        type: Number,
+        required: true,
+      },
+      longitude: {
+        type: Number,
+        required: true,
+      },
+    },
+  },
+  { collection: 'users_list' }
+);
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
