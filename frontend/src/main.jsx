e44076777d9bf164617e59d837988bc41ad28389
brianthich <thichbrian@gmail.com>
2026-02-08 17:03:23 -0800

    Renamed main to Main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './main.css';
+import './Main.css';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
